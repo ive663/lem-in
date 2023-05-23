d748b04f8b866b20ee425289ebc299988cb86401
ive663 <amayev.artyom@gmail.com>
2023-05-24 00:02:44 +0600

    added new test file

--- pathfinder.go
+++ pathfinder.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"math"
-	"strconv"
 	"strings"
 )
 
@@ -59,113 +56,6 @@ func isContain(arr []string, target string) bool {
 	return false
 }
 
-type Farm struct {
-	antAmount string
-	start     string
-	end       string
-	links     []string
-	rooms     []string
-}
-
-type Vertex struct {
-	Name string
-	X    int
-	Y    int
-}
-
-type UpdatedFarm struct {
-	AntAmount     int
-	Start         Vertex
-	End           Vertex
-	AdjacencyList map[string][]string
-	Rooms         map[string]Vertex
-}
-
-// return empty struct in error
-func TransformToVertex(data string) Vertex {
-	var result Vertex
-	splittedData := strings.Split(data, " ")
-	if len(splittedData) == 0 {
-		return Vertex{}
-	}
-
-	result.Name = splittedData[0]
-
-	var err error
-	result.X, err = strconv.Atoi(splittedData[1])
-	if err != nil {
-		return Vertex{}
-	}
-
-	result.Y, err = strconv.Atoi(splittedData[2])
-	if err != nil {
-		return Vertex{}
-	}
-
-	return result
-}
-
-func UpdateFarm(raw_farm Farm) (result UpdatedFarm, err error) {
-	result.AntAmount, err = strconv.Atoi(raw_farm.antAmount)
-	if err != nil {
-		return result, fmt.Errorf("UpdateFarm: %w", err)
-	}
-
-	var emptyV Vertex
-	result.Start = TransformToVertex(raw_farm.start)
-	if result.Start == emptyV {
-		return UpdatedFarm{}, fmt.Errorf("UpdateFarm: can't transform start field of Farm")
-	}
-
-	result.End = TransformToVertex(raw_farm.end)
-	if result.Start == emptyV {
-		return UpdatedFarm{}, fmt.Errorf("UpdateFarm: can't transform end field of Farm")
-	}
-
-	result.AdjacencyList = TransformToAdjacencyList(raw_farm.links)
-	if len(result.AdjacencyList) == 0 {
-		return UpdatedFarm{}, fmt.Errorf("UpdateFarm: can't transform links field of Farm")
-	}
-
-	result.Rooms = make(map[string]Vertex)
-	for _, elem := range raw_farm.rooms {
-		var newVertex Vertex = TransformToVertex(elem)
-		if newVertex == emptyV {
-			return UpdatedFarm{}, fmt.Errorf("UpdateFarm: can't transform rooms field of Farm")
-		}
-
-		result.Rooms[newVertex.Name] = newVertex
-	}
-
-	return result, nil
-}
-
-func main() {
-	farm := Farm{
-		antAmount: "3",
-		start:     "1 23 3",
-		end:       "0 9 5",
-		links: []string{"0-4", "0-6", "1-3", "4-3", "5-2", "3-5",
-			"4-2", "2-1", "7-6", "7-2", "7-4", "6-5"},
-		rooms: []string{"2 16 7", "3 16 3", "4 16 5", "5 9 3", "6 1 5", "7 4 8"},
-	}
-
-	newFarm, err := UpdateFarm(farm)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	//fmt.Println(newFarm)
-	// visited := DFS(newFarm.AdjacencyList, "1", []string{})
-	// fmt.Println(visited)
-	path, err := DijkstraAlgo(newFarm)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	fmt.Println(path)
-}
-
 func DistanceBetweenVertex(first Vertex, second Vertex) float64 {
 	xAxes := math.Pow(float64(first.X), 2) - math.Pow(float64(second.X), 2)
 	yAxes := math.Pow(float64(first.Y), 2) - math.Pow(float64(second.Y), 2)
@@ -175,7 +65,7 @@ func DistanceBetweenVertex(first Vertex, second Vertex) float64 {
 // Algorithm find shortest path in graph
 // between start and end
 // result path described like array of Vertexes
-func DijkstraAlgo(farm UpdatedFarm) ([]Vertex, error) {
+func DijkstraAlgo(farm Farm) ([]Vertex, error) {
 	var result []Vertex
 
 	currentVert := farm.Start
@@ -214,3 +104,29 @@ func DijkstraAlgo(farm UpdatedFarm) ([]Vertex, error) {
 
 	return result, nil
 }
+
+// func main() {
+// 	farm := Farm{
+// 		antAmount: "3",
+// 		start:     "1 23 3",
+// 		end:       "0 9 5",
+// 		links: []string{"0-4", "0-6", "1-3", "4-3", "5-2", "3-5",
+// 			"4-2", "2-1", "7-6", "7-2", "7-4", "6-5"},
+// 		rooms: []string{"2 16 7", "3 16 3", "4 16 5", "5 9 3", "6 1 5", "7 4 8"},
+// 	}
+
+// 	newFarm, err := UpdateFarm(farm)
+// 	if err != nil {
+// 		log.Fatalln(err.Error())
+// 	}
+
+// 	//fmt.Println(newFarm)
+// 	// visited := DFS(newFarm.AdjacencyList, "1", []string{})
+// 	// fmt.Println(visited)
+// 	path, err := DijkstraAlgo(newFarm)
+// 	if err != nil {
+// 		log.Fatalln(err.Error())
+// 	}
+
+// 	fmt.Println(path)
+// }
